6a4bbbcaefe75c484cbd4ab3fcda6976b410231b
Djama-Mo <djamamosss@gmail.com>
2021-03-16 13:04:28 +0600

    make it better

--- redis_cache.py
+++ redis_cache.py
@@ -3,8 +3,8 @@ from urllib.parse import urlparse
 import redis
 
 
-REDIS_URL = 'rediss://:pf41ce38d95403b8ab2177eb2ffa1cccd20bc15d62923012efaf2548c33e07879@ec2-54-204-192-122.compute-1' \
-            '.amazonaws.com:20860'
+REDIS_URL = 'rediss://:p4ce29b6dee4edc3828979262eacdf0254511cfba3fe4f84ca55fdd83395a2e44@ec2-54-161-35-22.compute-1.' \
+            'amazonaws.com:27510'
 url = urlparse(REDIS_URL)
 rediska = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True,
                       ssl_cert_reqs=None)
